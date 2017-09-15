c112214125571efb750af95dcdfb52408a222c92
澜嵚 <814184517@qq.com>
2017-09-15 18:02:39 +0800

    设计 propTypes‘

--- src/index.js
+++ src/index.js
@@ -4,20 +4,73 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './index.less';
 
-class ReactSwipeLoad extends React.Component {
+class SwipeLoad extends React.Component {
+  componentDidMount() {
+    const bottomNode = this.refs.bottomNode;
+    // 计算底部提前加载距离
+    if(!!bottomNode && !this.props.threshold) {
+      // 默认滑到加载区2/3处时加载
+      this._threshold = Math.floor(bottomNode.scrollHeight * 1/3);
+    } else {
+      this._threshold = this.props.threshold;
+    }
+
+    // 获取文档高度
+    this._scrollContentHeight = document.body.scrollHeight;
+    // 获取win显示区高度
+    this._windowHeight = document.documentElement.clientHeight;
+
+    this.autoLoad();
+  }
+
+  // 如果文档高度不大于窗口高度，数据较少，自动加载下方数据
+  autoLoad() {
+    if(this.props.onBottomLoad && this.props.autoLoad) {
+      if((this._scrollContentHeight - this._threshold) <= this._windowHeight) {
+        this.loadDown();
+      }
+    }
+  }
+
+  // 加载底部更多内容
+  loadDown() {
+    this.props.onBottomLoad && this.props.onBottomLoad();
+  }
+
   render() {
+    const { children, onBottomLoad, bottomNode } = this.props;
+
     return (
-      <div className="uniform-cpnt-ReactSwipeLoad">
-        Please edit src/index.js to update this component!
+      <div>
+        {children}
+        <div ref="bottomNode">
+        { // 如果需要底部加载，提前在下方插入 dom
+          onBottomLoad ? bottomNode['pulling'] : '' // 上拉阶段
+        }
+        </div>
       </div>
     );
   }
 }
 
-ReactSwipeLoad.propTypes = {
-
+SwipeLoad.propTypes = {
+  onTopRefresh: PropTypes.func, // 下拉刷新回调函数
+  onBottomLoad: PropTypes.func, // 上拉加载回调函数
+  threshold: PropTypes.number, // 底部提前加载的距离
+  topNode: PropTypes.object, // 页面顶部插入的节点，在不同状态下展示不同内容
+  bottomNode: PropTypes.object, // 页面底部插入的节点
+  autoLoad: PropTypes.bool // 数据不足一屏时是否自定加载
 };
 
-ReactSwipeLoad.displayName = 'ReactSwipeLoad';
+SwipeLoad.defaultProps = {
+  autoLoad: true,
+  bottomNode: {
+    pulling: <div>↑上拉加载更多</div>,
+    loading: <div>上拉加载中...</div>,
+    noData: <div>没有更多了</div>
+  }
+};
 
-export default ReactSwipeLoad;
+SwipeLoad.displayName = 'SwipeLoad';
+
+export default SwipeLoad;
--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,86 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactSwipeLoad from './index';
+import SwipeLoad from './index';
+import './index.test.less';
+
+const mockData = [
+  {
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-09-14/c47d007485407d06fa4b84b10da3627d.jpg'
+  }
+];
+
+const mockData2 = [
+  {
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  },{
+    text: '测试数据测试数据测试数据',
+    img: 'http://ykimg.alicdn.com/develop/image/2017-06-15/bccb7d17a4b1dbd378469be59ed4383d.jpg'
+  }
+];
 
 class Test extends React.Component {
+
+  onBottomLoad = () => {
+    console.log('on bottom load');  // eslint-disable-line
+  }
+
   render() {
-    return <ReactSwipeLoad />;
+    const porps = {
+      onBottomLoad: this.onBottomLoad
+    };
+
+    return  <SwipeLoad {...porps}>
+              {
+                mockData.map((item, index) => (
+                  <div className="item" key={index} href="#">
+                    <h3>{item.text + '  ' +  index}</h3>
+                    <img src={item.img} alt="" />
+                  </div>
+                ))
+              }
+            </SwipeLoad>;
   }
 }
 
